0600f00a387cbd722abb7ea27f56de6cf55ecee9
RadhikaK14 <62436185+RadhikaK14@users.noreply.github.com>
2021-07-30 12:03:17 +0100

    Updated table solution

--- multiplication_table.py
+++ multiplication_table.py
@@ -5,15 +5,21 @@
 #         if i<10:
 #             print()
 
+# kata accepted solution 
+# def multi_table(number):
+#     y = ''
+#     for i in range(1,11):
+#         answer = i*number
+#         x = '{}*{}={}'.format(i, number, answer)
+#         if i <10:
+#             x+='\n'
+#         y = y + x
+#     return y
+
+# alternate solution
+
 def multi_table(number):
-    y = ''
-    for i in range(1,11):
-        answer = i*number
-        x = '{}*{}={}'.format(i, number, answer)
-        if i <10:
-            x+='\n'
-        y = y + x
-    return y
+    return '\n'.join(f'{i} * {number} = {i * number}' for i in range(1, 11))
 
 table = multi_table(5)
 print(table)
